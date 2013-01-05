da05e12b5788001a79980c35d0b46e1df16f7546
Javier Arce <jarce@arcenet.com>
2013-01-05 09:42:17 -0600

    init

---
+++ project/Build.scala
@@ -0,0 +1,19 @@
+import sbt._
+import Keys._
+import PlayProject._
+import cloudbees.Plugin._
+
+object ApplicationBuild extends Build {
+
+    val appName         = "hellow"
+    val appVersion      = "1.0-SNAPSHOT"
+
+    val appDependencies = Seq(
+      // Add your project dependencies here,
+    )
+
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(cloudBeesSettings :_*)
+    .settings(
+  CloudBees.applicationId := Some("hellow")
+)
+}
