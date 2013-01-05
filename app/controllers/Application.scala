20daf74495e02f80100df5ce67e27890e6bc6454
Javier Arce <jarce@arcenet.com>
2013-01-05 13:58:46 -0600

    H2SQL_IMPLEMENTED

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,23 +13,24 @@ object Application extends Controller {
   }
 
   def tasks = Action {
-  Ok(views.html.index(Task.all(), taskForm))
-}
+    Ok(views.html.index(Task.all(), taskForm))
+  }
 
   def newTask = Action { implicit request =>
-  taskForm.bindFromRequest.fold(
-    errors => BadRequest(views.html.index(Task.all(), errors)),
-    label => {
-      Task.create(label)
-      Redirect(routes.Application.tasks)
-    }
-  )
-}
+    taskForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(Task.all(), errors)),
+      label => {
+        Task.create(label)
+        Redirect(routes.Application.tasks)
+      })
+  }
 
-  def deleteTask(id: Long) = TODO
-  
+  def deleteTask(id: Long) = Action {
+    Task.delete(id)
+    Redirect(routes.Application.tasks)
+  }
+
   val taskForm = Form(
-  "label" -> nonEmptyText
-)
+    "label" -> nonEmptyText)
 
 }
